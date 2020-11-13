fc111bfafd980ca4f0dedea732ec2eb8337b8d03
Taynara Garces de Lima <taynaragarces@gmail.com>
2020-11-13 13:06:54 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabcontent = document.querySelectorAll('.js-tabcontent section');
+ 
+
+function activeTab(index){
+    tabcontent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+
+    tabcontent[index].classList.add('ativo');
+ }
+
+ tabmenu.forEach((itemMenu, index)=>{
+        itemMenu.addEventListener('click', () =>{
+            activeTab(index);
+        })
+ });
