bd6e6822745bf3773af605a9f644db5a0a3a0b51
Taynara <taynaragarces@gmail.com>
2020-11-17 15:32:29 -0300

    slide animado

--- script.js
+++ script.js
@@ -64,3 +64,28 @@ linksInternos.forEach((link)=>{
 }
 
 initScrollSuave();
+
+function initAnimaScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.6;
+        function animaScroll(){
+            sections.forEach((section)=>{
+                const sectionTop = section.getBoundingClientRect().top -  windowMetade;
+                if(sectionTop < 0){
+                    section.classList.add('ativo');
+                }
+            });
+        }
+        
+        animaScroll();
+        
+        window.addEventListener('scroll', animaScroll);
+        
+    }
+}
+
+initAnimaScroll();
+
+
